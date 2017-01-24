7d73d199c240e60277ee890e29a128e879198928
Christina <christinasc@gmail.com>
2017-01-23 22:11:41 -0800

    clean

--- rds_config.py
+++ rds_config.py
@@ -3,5 +3,5 @@
 #config file containing credentials for rds mysql instance
 db_username = "farallon"
 db_password = "iceoasis"
-db_name = "ebdb"
-db_endpoint = "aau22sqmew94ym.csniiwsrgjcu.us-west-2.rds.amazonaws.com"
+db_name = "ebdb123"
+db_endpoint = "mysqlforlambdatest.csniiwsrgjcu.us-west-2.rds.amazonaws.com"
